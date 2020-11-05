5152a6308c8011848d55fec2bbc71ae1f8c80b46
HolyLoki <forwoermind@gmail.com>
2020-11-05 16:01:40 +0600

    added new thingsd

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function makeDarkOrLight() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+function showPhoneNumber(){
+    alert("Call this number: +996 525235236");
+}
